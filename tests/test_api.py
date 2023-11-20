616ba5d3dcbaff9c159001c0b3f12758d30220ee
Nezih Akin <ismail.akin@ing.com>
2023-11-21 00:53:50 +0100

    all pytest issues solved

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,11 +10,11 @@ def test_say_hello():
     assert request.json() == {"greeting":"Welcome to my Model!"}
 
 def test_inference_1(pred_label_1):
-    request = client.post("/inference/", json=pred_label_1)
+    request = client.post("/inference", json=pred_label_1)
     assert request.status_code == 200
     assert request.json() == {"Prediction": "<=50K"}
 
 def test_inference_2(pred_label_2):
-    request = client.post("/inference/", json=pred_label_2)
+    request = client.post("/inference", json=pred_label_2)
     assert request.status_code == 200
     assert request.json() == {"Prediction": ">50K"}
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,24 +1,11 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 
-from starter.ml.data import process_data
 from starter.ml.model import compute_model_metrics, inference, train_model
 
 def test_train_model(data):
     X, y = data
     model = train_model(X,y)
     assert isinstance(model, RandomForestClassifier)
-
-def test_process_data(data, cat_features):
-    train, _ = train_test_split(data, test_size=0.20)
-
-    X_train, y_train, _, _ = process_data(
-        train, categorical_features=cat_features, label="salary", training=True
-    )
-
-    assert X_train.size > 0
-    assert y_train.size > 0
-
 
 def test_inference(model, data):
     X_test, _ = data
